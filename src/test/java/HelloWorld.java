d6869d3cb4a758d2c080797cd3e13a9060a6507d
Evgeniy Sheludiakov <sheludiakovev@mail.ru>
2023-08-22 00:25:47 +0500

    Ответ к задаче №4

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorld {
+
+    //Задание 4
+    @Test
+    public  void helloWorld(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
