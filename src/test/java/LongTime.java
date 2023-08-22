21590c4da6bfbace1877f0026d2c597dd8f90fe1
Evgeniy Sheludiakov <sheludiakovev@mail.ru>
2023-08-22 23:29:39 +0500

    Задание №8 token

---
+++ src/test/java/LongTime.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongTime {
+
+    //Задание №8
+    @Test
+    public  void redirectTest() throws InterruptedException {
+        JsonPath response1 = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        response1.prettyPrint();
+        String token = response1.get("token");
+        int second = response1.get("seconds");
+        int mi = second * 1000;
+        System.out.println(mi);
+        System.out.println(token);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("token", token);
+
+        Response response2 = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        response2.prettyPrint();
+
+        Thread.sleep(mi);
+
+        Response response3 = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        response3.prettyPrint();
+    }
+}
