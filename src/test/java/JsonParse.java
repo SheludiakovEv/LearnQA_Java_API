0b4ba140571672d0161c1d6889051a3bc7ad4f0c
Evgeniy Sheludiakov <sheludiakovev@mail.ru>
2023-08-22 22:11:40 +0500

    Задание №5 парсинг json

---
+++ src/test/java/JsonParse.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class JsonParse {
+
+    //Задание №5
+    @Test
+    public  void getJson(){
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        response.prettyPrint();
+        String message = response.getString("messages[1].message");
+        System.out.println("\nВыводим второе сообщение:" + message);
+    }
+}
