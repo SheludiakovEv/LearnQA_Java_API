55a62aa444f5d398f55f0dcf005ea0f9fa70165d
Evgeniy Sheludiakov <sheludiakovev@mail.ru>
2023-08-28 22:42:14 +0500

    Задание №11 Проверка куки

---
+++ src/test/java/CookieCheck.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class CookieCheck {
+
+    //Задание №11
+String regex = "^\\w+_\\w+$";
+
+    @Test
+    public void cookieTest() {
+        Response response1 = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        String header = response1.getCookie("HomeWork");
+        Assert.assertTrue(header.matches(regex));
+        Assert.assertEquals(header, "hw_value");
+        System.out.println(header);
+    }
+}
