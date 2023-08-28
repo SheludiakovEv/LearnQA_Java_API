a3f2066ccca8f328f50f894d39ca537eb393c581
Evgeniy Sheludiakov <sheludiakovev@mail.ru>
2023-08-28 22:50:47 +0500

    Задание №12 Проверка секретного заголовка

---
+++ src/test/java/HeadersCheck.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class HeadersCheck {
+
+    //Задание №12
+    String secret = "Some secret value";
+
+    @Test
+    public void cookieTest() {
+        Response response1 = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+        String headers = response1.getHeader("x-secret-homework-header");
+        Assert.assertEquals("Значение заголовка не отличается", headers, secret);
+        System.out.println(headers);
+    }
+}
